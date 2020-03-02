9cb7626942d0c9da97dd49c7828218d62ca51c8b
merewelch214 <merewelch@gmail.com>
2020-03-02 11:59:12 -0500

    fix user auth

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,7 +2,7 @@ class AuthController < ApplicationController
 
     def login
         user = User.find_by(username: params[:username])
-        if user && user.authenticate(password: params[:password])
+        if user && user.authenticate(params[:password])
             render json: user
         else 
             render json: {errors: 'Incorrect username or password'}
